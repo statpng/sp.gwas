076b65cdcf7cfb773e5e5325d66fcf6af73a614f
statpng <kkp7700@naver.com>
2019-03-11 09:09:43 +0900

    add example data

--- R/sp.gwas-package.R
+++ R/sp.gwas-package.R
@@ -6,16 +6,13 @@
 #' @details The penalty function of \code{elastic-net} is defined as \deqn{\alpha||\beta||_1+(1-\alpha)||\beta||_2/2,} where \eqn{\alpha} is a mixing proportion of ridge and the lasso, and \eqn{\beta} is regression coefficients. This penalty is equivalent to the Lasso penalty if \code{alpha=1}. \cr
 #' @return A list of data files(genotype, phenotype, etc.), results for selection probabilities, and manhattan plot for multiple traits.
 
-#' @import glmnet CMplot stats ggplot2 utils
-#' @importFrom tidyr gather
+#' @import glmnet CMplot stats ggplot2 utils tidyr dplyr
 #' @importFrom gridExtra grid.arrange
 #' @importFrom compiler cmpfun
 #' @importFrom bestNormalize boxcox
 #' @importFrom writexl write_xlsx
 #' @importFrom readxl read_xlsx
 #' @importFrom data.table fread
-#' @importFrom dplyr "%>%"
-#' @importFrom dplyr select
 
 #' @references Zou, H., & Hastie, T. (2005). Regularization and variable selection via the elastic net. Journal of the royal statistical society: series B (statistical methodology), 67(2), 301-320.
 
