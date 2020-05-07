b13ad318e43c6204b9730f18c63f3e4861563f4c
statpng <kkp7700@gmail.com>
2020-05-07 10:14:04 +0900

    simplify importing procedure of phenotype

--- R/import.hapmap.R
+++ R/import.hapmap.R
@@ -21,84 +21,84 @@ import.hapmap <-
     if( any( class(phenotype) %in% c("tbl", "tbl_df") ) ){
       phenotype <- as.data.frame(phenotype)
     }
-
-  # Import a phenotype data -------------------------------------------------
-  if( length( dim(phenotype) ) > 0 | input.type=="object" ){
-    myY.init <- phenotype
-  } else {
-    # if(is.null(phenotype)) {
-    #   print("Select the path of phenotype data which has sample IDs in its first column.")
-    #   phenotype <- choose.files()
-    # }
-    if( grepl("*.xlsx", phenotype) ){
-      myY.init <- read_excel(phenotype)
-    } else if( grepl("*.csv|*.txt", phenotype) ){
-      myY.init <- as.data.frame( fread(phenotype), stringsAsFactors=FALSE )
-    } else {
-      myY.init <- as.data.frame( fread(phenotype), stringsAsFactors=FALSE )
-    }
-  }
-  
-  
-  # Restriction of the number of phenotypes ---------------------------------
-  if( ncol(myY.init) > 5 & is.null(y.col) ){
-    stop("The number of phenotypes must be equal or less than 4.")
-  } else if (!is.null(y.col)){
-    myY.init <- myY.init[,c(y.id.col, y.col)]
-  }
-  # myY.init <- myY.init[!duplicated(myY.init[,y.id.col]),]
-  
-  if( any(duplicated(myY.init[,1])) ){
-    cat( "Duplicated phenotype IDs: {", myY.init[,1][ duplicated(myY.init[,1]) ], "}\n" )
-    stop("There are duplicated sample IDs in phenotype data.")
-  }
-  
-  print("Checking whether the class of phenotype corresponds to family")
-  if( family == "gaussian" ){
-    for( j in 1:(ncol(myY.init)-1) ){
-      if( length( table( myY.init[,j+1] ) ) < 10 ) warning(paste0("The category of ", j, "-th phenotype is less than 5. Isn't it categorical variable?"))
-      if( class( myY.init[,j+1] ) != "numeric" ) stop("Please confirm classes of your phenotypes.")
-    }
-  }
-  
     
-  
-  
-# Import a genotype data --------------------------------------------------
+    # Import a phenotype data -------------------------------------------------
+    if( length( dim(phenotype) ) > 0 | input.type=="object" ){
+      myY.init <- phenotype
+    } else {
+      # if(is.null(phenotype)) {
+      #   print("Select the path of phenotype data which has sample IDs in its first column.")
+      #   phenotype <- choose.files()
+      # }
+      if( grepl("*.xlsx", phenotype) ){
+        myY.init <- read_excel(phenotype)
+      } else if( grepl("*.csv|*.txt", phenotype) ){
+        myY.init <- as.data.frame( fread(phenotype), stringsAsFactors=FALSE )
+      } else {
+        myY.init <- as.data.frame( fread(phenotype), stringsAsFactors=FALSE )
+      }
+    }
+    
+    
+    # Restriction of the number of phenotypes ---------------------------------
+    if( ncol(myY.init) > 5 & is.null(y.col) ){
+      stop("The number of phenotypes must be equal or less than 4.")
+    } else if (!is.null(y.col)){
+      myY.init <- myY.init[,c(y.id.col, y.col)]
+    }
+    # myY.init <- myY.init[!duplicated(myY.init[,y.id.col]),]
+    
+    if( any(duplicated(myY.init[,1])) ){
+      cat( "Duplicated phenotype IDs: {", myY.init[,1][ duplicated(myY.init[,1]) ], "}\n" )
+      stop("There are duplicated sample IDs in phenotype data.")
+    }
+    
+    print("Checking whether the class of phenotype corresponds to family")
+    if( family == "gaussian" ){
+      for( j in 1:(ncol(myY.init)-1) ){
+        if( length( table( myY.init[,j+1] ) ) < 10 ) warning(paste0("The category of ", j, "-th phenotype is less than 5. Isn't it categorical variable?"))
+        if( class( myY.init[,j+1] ) != "numeric" ) stop("Please confirm classes of your phenotypes.")
+      }
+    }
+    
+    
+    
+    
+    # Import a genotype data --------------------------------------------------
     if( length( dim(genotype) ) > 0 | input.type=="object" ){
       myX.init <- genotype
     } else {
       if(is.null(genotype)) {
-          print("Select genotype data with hapmap format.")
-          genotype <- choose.files()
+        print("Select genotype data with hapmap format.")
+        genotype <- choose.files()
       }
-  
+      
       if( grepl("*.xlsx", genotype) ){
-          myX.init <- read_excel(genotype, col_names=FALSE)
+        myX.init <- read_excel(genotype, col_names=FALSE)
       } else if( grepl("*.csv|*.txt", genotype) ){
-          myX.init <- as.data.frame( fread(genotype, header=FALSE), stringsAsFactors=FALSE )
+        myX.init <- as.data.frame( fread(genotype, header=FALSE), stringsAsFactors=FALSE )
       } else {
         myX.init <- as.data.frame( fread(genotype, header=FALSE), stringsAsFactors=FALSE )
       } 
     }
-  
-  if( any(duplicated(as.character( myX.init[1, 12:ncol(myX.init) ] ) )) ){
-    cat( "Duplicated genotype IDs: {", 
-         as.character( myX.init[1, 12:ncol(myX.init) ] )[
-           duplicated(as.character( myX.init[1, 12:ncol(myX.init) ] ) )
-         ],
-         "}\n" )
-    stop("There are duplicated sample IDs in genotype data.")
-  }
-  
-
-
-# Reorder the dataset according to the names ------------------------------
+    
+    if( any(duplicated(as.character( myX.init[1, 12:ncol(myX.init) ] ) )) ){
+      cat( "Duplicated genotype IDs: {", 
+           as.character( myX.init[1, 12:ncol(myX.init) ] )[
+             duplicated(as.character( myX.init[1, 12:ncol(myX.init) ] ) )
+             ],
+           "}\n" )
+      stop("There are duplicated sample IDs in genotype data.")
+    }
+    
+    
+    
+    # Reorder the dataset according to the names ------------------------------
     myX.init <- myX.init[,c(1:11, order( myX.init[1, 12:ncol(myX.init) ] )+11)]
     myY.init <- myY.init[order( myY.init[, 1] ), ]
-
-
-# Remove missing values from Y ---------------------------------------------------
+    
+    
+    # Remove missing values from Y ---------------------------------------------------
     
     if( remove.missingY ){
       print(paste0("Removing the missing values of phenotypes(",
@@ -108,9 +108,9 @@ import.hapmap <-
       
       myY.init <- myY.init[!apply(myY.init, 1, function(x) any(is.na(x))), ]
     }
-
     
-# Match the genotype with the phenotype -----------------------------------
+    
+    # Match the genotype with the phenotype -----------------------------------
     print("Matcing procedure between genotpe and phenotype")
     taxa.snp <- as.character( myX.init[1,12:ncol(myX.init), drop=TRUE] )
     taxa.phenotype <- myY.init[, 1]
@@ -129,8 +129,8 @@ import.hapmap <-
     
     
     
-
-# Quality Control ---------------------------------------------------------
+    
+    # Quality Control ---------------------------------------------------------
     png.maf <- function(genotype.hapmap, sep=""){
       
       # xx: vecor with c(GG, GC, CC, CC, CC)
@@ -141,7 +141,7 @@ import.hapmap <-
       for( i in 1:nrow(genotype.hapmap[-1,])){
         setTxtProgressBar(pb, i)
         xx <- as.character(unlist(genotype.hapmap[-1,-(1:11)][i,]))
-      
+        
         if(length(unique(xx))<=1) out[i] <- 0
         
         
@@ -196,7 +196,7 @@ import.hapmap <-
       
       out
     }
-
+    
     png.heterozygousCalls <- function(genotype.hapmap){
       
       set.heterozygote <- apply( subset( expand.grid(c("T","C","A","G"), c("T","C","A","G")), Var1 != Var2 ), 1, paste0, collapse="")
@@ -232,22 +232,22 @@ import.hapmap <-
     heterozygosity.final <- heterozygosity[filter.intersect]
     
     myX.init <- myX.init[c(1, filter.intersect+1), ]
-
-
-# Numericalize the coding of genotypes ------------------------------------
+    
+    
+    # Numericalize the coding of genotypes ------------------------------------
     print("Performing the numericalization procedure for genotpe data.")
     myGD.init <- suppressWarnings( as.data.frame( apply(myX.init[-1,-(1:11)], 1, function(one) GAPIT.Numericalization(one, bit=2, impute="Middle", Major.allele.zero=TRUE)), stringsAsFactors = FALSE ) )
     myGM.init <- myX.init[,1:4]
     myGT.init <- myX.init[,c(12:ncol(myX.init))]
     colnames(myGD.init) <- myX.init[-1,1]
     rownames(myGD.init) <- myX.init[1,-c(1:11)]
-
+    
     missing.snp <- apply( myX.init[-1,-(1:11)], 1, function(X) sum(is.na(X)) )
     missing.sample <- apply( myX.init[-1,-(1:11)], 2, function(X) sum(is.na(X)) )
-
+    
     write.csv(x=data.frame(ID=myX.init[-1,1], missing.snp, stringsAsFactors = FALSE), file=paste0(save.path,"/[0]missing.snp.csv"), row.names=FALSE, fileEncoding = "UTF-8")
     write.csv(x=data.frame(ID=as.character(myX.init[1,-(1:11)]), missing.sample, stringsAsFactors = FALSE), file=paste0(save.path,"/[0]missing.sample.csv"), row.names=FALSE, fileEncoding = "UTF-8")
-
+    
     # Saving Excel But its time is too long ----------------------------------
     # for( Excelobj in list(myX, myGD, myGM, myGT) ){
     #     write.xlsx(x = Excelobj,
@@ -256,8 +256,8 @@ import.hapmap <-
     #                append=T )
     # }
     # End ---------------------------------------------------------------------
-
-
+    
+    
     if(identical(as.character(myX.init[1,ss.x+11]), as.character(myY.init[ss.y, 1]) )){
       myX <- as.data.frame( myX.init[,c(1:11,ss.x+11)], stringsAsFactors=FALSE )
       myGD <- myGD.init[ss.x,]
@@ -273,44 +273,44 @@ import.hapmap <-
       stop("Sample IDs between genotypes and phenotypes do not match")
     }
     
-
+    
     # myX[ss.x+11,][1:10,1:10]
     # myY[ss.y,][1:10,]
     if( family=="gaussian" ){
-
-    myY.original <- myY
-    # for( j in 1:(ncol(myY)-1) ){
-    #   myY.original[,j+1] <- as.numeric(as.character(myY.original[,j+1]))
-    # }
-    if( normalization ){
-      print("Performing the normalization for phenotypes, but there is no evidence that they can be normalized.")
       
-      for( j in 1:(ncol(myY)-1) ){
-        myY[,j+1] <- boxcox(myY.original[,j+1], standardize = FALSE)$x.t
+      myY.original <- myY
+      # for( j in 1:(ncol(myY)-1) ){
+      #   myY.original[,j+1] <- as.numeric(as.character(myY.original[,j+1]))
+      # }
+      if( normalization ){
+        print("Performing the normalization for phenotypes, but there is no evidence that they can be normalized.")
+        
+        for( j in 1:(ncol(myY)-1) ){
+          myY[,j+1] <- boxcox(myY.original[,j+1], standardize = FALSE)$x.t
+        }
+      } else {
+        myY <- myY.original
       }
-    } else {
-      myY <- myY.original
-    }
-    
-    norm.pvalue.original <- norm.pvalue <- NULL
-    for( j in 1:(ncol(myY.original)-1) ){
+      
+      norm.pvalue.original <- norm.pvalue <- NULL
+      for( j in 1:(ncol(myY.original)-1) ){
         norm.pvalue.original[j] <- round( shapiro.test(myY.original[,j+1])$p.value, 4)
         norm.pvalue[j] <- round( shapiro.test(myY[,j+1])$p.value, 4)
-    }
-
+      }
+      
       if( normalization ){
         Hist.y.original <- myY.original %>%
-            .[,-1,drop=F] %>%
-            gather(PHENOTYPE) %>%
-            data.frame(., pvalue=rep(norm.pvalue.original, each=nrow(.)/length(norm.pvalue.original))) %>%
-            ggplot(aes(value)) +
-            geom_histogram(fill="white", colour="black") +
-            xlab("Original Phenotypes") +
-            ggtitle(paste0("Histogram of original phenotypes")) +
-            theme_bw() +
-            facet_wrap(~PHENOTYPE, nrow=2, scales="free") +
-            geom_label(x=Inf, y=Inf, aes(label=pvalue),
-                       vjust = "inward", hjust = "inward")
+          .[,-1,drop=F] %>%
+          gather(PHENOTYPE) %>%
+          data.frame(., pvalue=rep(norm.pvalue.original, each=nrow(.)/length(norm.pvalue.original))) %>%
+          ggplot(aes(value)) +
+          geom_histogram(fill="white", colour="black") +
+          xlab("Original Phenotypes") +
+          ggtitle(paste0("Histogram of original phenotypes")) +
+          theme_bw() +
+          facet_wrap(~PHENOTYPE, nrow=2, scales="free") +
+          geom_label(x=Inf, y=Inf, aes(label=pvalue),
+                     vjust = "inward", hjust = "inward")
         
         Hist.y <- myY %>%
           .[,-1,drop=FALSE] %>%
@@ -323,7 +323,7 @@ import.hapmap <-
           facet_wrap(~PHENOTYPE, nrow=2, scales="free") +
           geom_label(x=Inf, y=Inf, aes(label=pvalue),
                      vjust = "inward", hjust = "inward")
-      
+        
       } else {
         Hist.y <- myY %>%
           .[,-1,drop=FALSE] %>%
@@ -337,20 +337,17 @@ import.hapmap <-
           geom_label(x=Inf, y=Inf, aes(label=pvalue),
                      vjust = "inward", hjust = "inward")
       }
-    
-    
-    
-    } else if (family!="gaussian"){
-      myY <- myY.original
+      
+      if( normalization ){
+        Hist.norm <- grid.arrange(Hist.y.original, Hist.y, nrow=1, heights=15)
+        ggsave(Hist.norm, filename = paste0(save.path,"/[1]Hist.phenotype.norm.pdf"), width = 10, height = 5)
+      } else {
+        Hist.norm <- grid.arrange(Hist.y, nrow=1, heights=15)
+        ggsave(Hist.norm, filename = paste0(save.path,"/[1]Hist.phenotype.norm.pdf"), width = 10, height = 5)
+      }
+      
     }
     
-    if( normalization ){
-      Hist.norm <- grid.arrange(Hist.y.original, Hist.y, nrow=1, heights=15)
-      ggsave(Hist.norm, filename = paste0(save.path,"/[1]Hist.phenotype.norm.pdf"), width = 10, height = 5)
-    } else {
-      Hist.norm <- grid.arrange(Hist.y, nrow=1, heights=15)
-      ggsave(Hist.norm, filename = paste0(save.path,"/[1]Hist.phenotype.norm.pdf"), width = 10, height = 5)
-    }
     
     write.csv(x=myX, file=paste0(save.path,"/[1]myX.csv"), row.names=FALSE, fileEncoding = "UTF-8")
     if(family=="gaussian"){
@@ -376,8 +373,8 @@ import.hapmap <-
     
     
     save( myDATA, file=paste0(save.path,"/[1]Data",".RData"))
-
+    
     # End ---------------------------------------------------------------------
-
+    
     return( myDATA )
-}
+  }
